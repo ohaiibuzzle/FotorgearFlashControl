51ae6ae01c49f7b4eb2239f91dac61ed51c42a1b
ohaiibuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2026-06-06 23:36:33 +0700

    fix: update gradle

--- app/src/main/java/dev/ohaiibuzzle/flashcontrol/ui/FlashControlScreen.kt
+++ app/src/main/java/dev/ohaiibuzzle/flashcontrol/ui/FlashControlScreen.kt
@@ -27,11 +27,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import dev.ohaiibuzzle.flashcontrol.accessibility.AccessibilityFlashSettingsStore
 import dev.ohaiibuzzle.flashcontrol.accessibility.FlashAccessibilityServiceState
 import dev.ohaiibuzzle.flashcontrol.accessibility.TapPointPickerActivity
